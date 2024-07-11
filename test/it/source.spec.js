d0df5c519a078e26b805b2e4a8f053edc9508a98
Bryan Stopp <bryan.stopp@gmail.com>
2024-07-11 11:55:13 -0400

    Add integration test for listing sources.

--- test/it/source.spec.js
+++ test/it/source.spec.js
@@ -13,10 +13,8 @@
 import assert from 'node:assert';
 import { destroyMiniflare, getMiniflare } from '../mocks/miniflare.js';
 import worker from '../../src/index.js';
-import { SignJWT } from 'jose';
 
-
-describe('Org Requests', async () => {
+describe('Source Requests', async () => {
   let mf;
   let env;
   beforeEach(async () => {
@@ -27,30 +25,14 @@ describe('Org Requests', async () => {
     await destroyMiniflare(mf);
   });
 
-  describe('list', () => {
-    it('lists orgs for anonymous user', async () => {
-      const req = new Request('https://admin.da.live/list');
+  describe('list', async() => {
+    it('lists content', async () => {
+      const req = new Request('https://admin.da.live/list/wknd');
       const resp = await worker.fetch(req, env);
       assert.strictEqual(resp.status, 200);
       const body = await resp.json();
       assert.strictEqual(body.length, 1);
-      assert.strictEqual(body[0].name, 'wknd');
+      assert.deepStrictEqual(body[0], {  name: 'index', ext: 'html', path: '/wknd/index.html' });
     });
-
-    it('lists orgs for authenticated user', async () => {
-      const secret = new TextEncoder().encode('secret');
-      const mockPayload = { user_id: 'aparker@geometrixx.info', created_at: Date.now(), expires_in: 300000 };
-      const jws = await new SignJWT(mockPayload)
-        .setProtectedHeader({ alg: 'HS256' })
-        .sign(secret);
-
-      const req = new Request('https://admin.da.live/list', { headers: { 'Authorization': `Bearer ${jws}` } });
-      const resp = await worker.fetch(req, env);
-      assert.strictEqual(resp.status, 200);
-      const body = await resp.json();
-      assert.strictEqual(body.length, 2);
-      assert(body.some((item) => item.name === 'wknd'));
-      assert(body.some((item) => item.name === 'geometrixx'));
-    });
-  });
+  })
 });
