7080976db160276784114dabaf4d7b6abba87691
David Bosschaert <bosschae@adobe.com>
2025-02-05 15:08:47 +0000

    Return 403 when logged in but not authorized

--- src/index.js
+++ src/index.js
@@ -24,7 +24,10 @@ export default {
 
     const daCtx = await getDaCtx(req, env);
     const { authorized, key } = daCtx;
-    if (!authorized) return daResp({ status: 401 });
+    if (!authorized) {
+      const status = daCtx.users[0].email === 'anonymous' ? 401 : 403;
+      return daResp({ status });
+    }
     if (key?.startsWith('.da-versions')) return daResp({ status: 404 });
 
     let respObj;
--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,5 @@
 import assert from 'assert';
+import esmock from 'esmock';
 import handler from '../src/index.js';
 
 describe('fetch', () => {
@@ -14,5 +15,31 @@ describe('fetch', () => {
   it('should return a response object for unknown', async () => {
     const resp = await handler.fetch({ url: 'https://www.example.com', method: 'BLAH' }, {});
     assert.strictEqual(resp.status, 501);
+  });
+
+  it('should return 401 when not authorized and not logged in', async () => {
+    const hnd = await esmock(
+      '../src/index.js', {
+        '../src/utils/daCtx.js': {
+          default: async () => ({ authorized: false, users: [{ email: 'anonymous' }] })
+        }
+      }
+    )
+
+    const resp = await hnd.fetch({ method: 'GET' }, {});
+    assert.strictEqual(resp.status, 401);
+  });
+
+  it('should return 403 when logged in but not authorized', async () => {
+    const hnd = await esmock(
+      '../src/index.js', {
+        '../src/utils/daCtx.js': {
+          default: async () => ({ authorized: false, users: [{ email: 'joe@bloggs.org' }] })
+        }
+      }
+    )
+
+    const resp = await hnd.fetch({ method: 'GET' }, {});
+    assert.strictEqual(resp.status, 403);
   });
 });
