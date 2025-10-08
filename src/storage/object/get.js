ec67433f568b174d35c1ed3ca82f27d69f2dd703
Alexandre Capt <acapt@adobe.com>
2025-10-08 09:38:04 +0200

    chore: log gzip encoded content (#181)
    * chore: log gzip encoded content
    
    * chore: c8 ignore
    
    * chore: fix c8

--- src/storage/object/get.js
+++ src/storage/object/get.js
@@ -31,6 +31,16 @@ export default async function getObject(env, { bucket, org, key }, head = false)
   if (!head) {
     try {
       const resp = await client.send(new GetObjectCommand(input));
+
+      /* c8 ignore start */
+      if (resp.ContentEncoding === 'gzip') {
+        // log to track which documents are gzip encoded and run scripts to fix them
+        // eslint-disable-next-line no-console
+        console.warn('Content is gzip encoded - request might fail');
+        throw new Error('Corrupted content');
+      }
+      /* c8 ignore end */
+
       return {
         body: resp.Body,
         status: resp.$metadata.httpStatusCode,
