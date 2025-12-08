6ec19b660f94c593903bbd0b2d68f86582717fe0
kptdobe <acapt@adobe.com>
2025-12-08 16:25:45 +0100

    chore: let's break it!

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import getDaCtx from './utils/daCtx.js';
 import daResp from './utils/daResp.js';
 
 import headHandler from './handlers/head.js';
-import getHandler from './handlers/get.js';
+// import getHandler from './handlers/get.js';
 import postHandler from './handlers/post.js';
 import deleteHandler from './handlers/delete.js';
 
@@ -54,7 +54,7 @@ export default {
         respObj = await headHandler({ env, daCtx });
         break;
       case 'GET':
-        respObj = await getHandler({ env, daCtx });
+        // respObj = await getHandler({ env, daCtx });
         break;
       case 'PUT':
         respObj = await postHandler({ req, env, daCtx });
